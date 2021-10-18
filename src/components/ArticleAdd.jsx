1f8ed652cc37ee1ed4bc88523230f67481e5b4e2
sumit kumar bighaniya <sb78639@gmail.com>
2021-10-18 17:04:34 +0530

    Create ArticleAdd.jsx

---
+++ src/components/ArticleAdd.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+const ArticleAdd = ()=>{
+  return (
+    
+     <ins className="adsbygoogle"
+     style={{"display":"block"}}
+     data-ad-client="ca-pub-3904291439301971"
+     data-ad-slot="4698777502"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+    
+    )
+}
