2c630be77ce4cbae4e288e2ccb943771912fe086
sumit kumar bighaniya <sb78639@gmail.com>
2021-10-18 19:53:20 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import Modal from '../components/Modal'
 import {MdExpandLess} from "react-icons/md"
 import { connect } from 'react-redux'
 import { setNav } from '../redux/actions/_appActions'
-import ArticleAdd from "../components/ArticleAdd"
+// import ArticleAdd from "../components/ArticleAdd"
 
 function Home(props) {
     const [scroll,setScroll] = React.useState(0);
@@ -35,7 +35,7 @@ function Home(props) {
             <div className="container">
             <Navbar/>
                 
-                <ArticleAdd/>
+//                 <ArticleAdd/>
             {props.nav && <div className="mobile__nav">
                 <div className="nav__links">
                 <a href="#work">WORK</a>
