66be6655e01fea74b27c905978bddf62f5c26011
Sumit kumar <sumitkumar@Sumits-MacBook-Air.local>
2022-04-25 13:40:20 +0530

    redesign added to dev

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,69 +1,112 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import Navbar from '../components/Navbar'
-import Work from '../components/Work'
-import "./Home.css"
-import Contact from '../components/Contact'
-import Resume from '../components/Resume'
-import Modal from '../components/Modal'
-import {MdExpandLess} from "react-icons/md"
-import { connect } from 'react-redux'
-import { setNav } from '../redux/actions/_appActions'
+import React from "react";
+import Navbar from "../components/Navbar";
+import { FaLinkedinIn, FaGithubSquare, FaInstagram } from "react-icons/fa";
+import "./Home.css";
+
+import { connect } from "react-redux";
+import { setNav } from "../redux/actions/_appActions";
+
+function WorkCard({ thumb, double, name }) {
+  console.log(thumb);
+  return (
+    <div className={`work_card ${double && "col-2"}`}>
+      <div
+        className="work_thumb"
+        style={{ backgroundImage: `url(${thumb})` }}
+      ></div>
+      <div className="work_card_overlay">
+        <div className="work_card_footer">
+          <span className="work-_chip">Featured</span>
+          <h2>{name}</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function BlackWorkCard() {
+  return (
+    <div className="work_card_blank">
+      <div className="work_thumb_blank"></div>
+      <div className="work_card_overlay_blank">
+        <div className="work_card_footer_blank">
+          <span className="work_chip"></span>
+          <div className="work_name"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
 // import ArticleAdd from "../components/ArticleAdd"
 
 function Home(props) {
-    const [scroll,setScroll] = React.useState(0);
-
-
-    const handleNav = (e)=>{
-        console.log(e.target);
-        if(!e.target.classList.contains("mobile__nav") && !e.target.classList.contains("mobile_nav_menu")){
-            props.setNav(false)
-        }
+  console.log(props.works);
+  const handleNav = (e) => {
+    console.log(e.target);
+    if (
+      !e.target.classList.contains("mobile__nav") &&
+      !e.target.classList.contains("mobile_nav_menu")
+    ) {
+      props.setNav(false);
     }
+  };
 
-    
-
-   
-
-
-
-    
-    return (
-        <div className="home" onClick={handleNav}>
-            <Modal/>
-            <div className="container">
-            <Navbar/>
-                
-
-            {props.nav && <div className="mobile__nav">
-                <div className="nav__links">
-                <a href="#work">WORK</a>
-                <a href="#resume">RESUME</a>
-                <a href="#contact">CONTACT</a>
-                </div>
-                
-            </div>}
-
-            
-            <Hero scroll={scroll} setScroll={setScroll}/>
-            <Work/>
-            <Contact/>
-            <Resume/>
-
-            <a href="#top" className={`goto_btn ${scroll>240 && "fab__enable"}`} style={{backgroundColor:props.data && props.data.color}}><MdExpandLess/></a>
-            </div>
+  return (
+    <div className="home" onClick={handleNav}>
+      <div className="container">
+        {props.data && <Navbar data={props.data && props.data} />}
+        <div className="hero_header">
+          <h1>
+            Motivation Is The First Step To <span>Success</span>
+          </h1>
+          <div className="social_profiles">
+            <a href="">
+              <FaLinkedinIn />
+            </a>
+            <a href="">
+              <FaGithubSquare />
+            </a>
+            <a href="">
+              <FaInstagram />
+            </a>
+          </div>
         </div>
-    )
+        <div className="sections">
+          <div className="work_section">
+            <span className="section_label">WORKS</span>
+            <div className="works">
+              {props.works.length > 0 ? (
+                props.works.map((work, index) => (
+                  <WorkCard
+                    key={index}
+                    thumb={work.thumb}
+                    double={index % 2 === 0}
+                    name={work.name}
+                  />
+                ))
+              ) : (
+                <>
+                  <BlackWorkCard />
+                  <BlackWorkCard />
+                  <BlackWorkCard />
+                  <BlackWorkCard />)
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
+const mapStateToProps = (state) => ({
+  data: state.appReducer.data,
+  nav: state.appReducer.nav,
+  works: state.appReducer.works,
+});
 
-const mapStateToProps = (state)=>({
-    data:state.appReducer.data,
-    nav:state.appReducer.nav
-})
-
-const mapDispatchToProps = (dispatch)=>({
-    setNav:(nav)=>dispatch(setNav(nav))
-})
-export default connect(mapStateToProps,mapDispatchToProps)(Home)
+const mapDispatchToProps = (dispatch) => ({
+  setNav: (nav) => dispatch(setNav(nav)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
